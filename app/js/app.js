f89d75d1a1d472fc44771fb6871082a867f83f71
Ashley Felton <ashley@ropable.com>
2013-04-04 20:42:56 +0800

    Minor diffs.

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 
 angular.module('wot1app', []).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/background', {templateUrl: 'partials/background.html', controller: BackgroundCtrl});
     $routeProvider.when('/new-game', {templateUrl: 'partials/new_game.html', controller: NewGameCtrl});
+    $routeProvider.when('/background', {templateUrl: 'partials/background.html', controller: BackgroundCtrl});
     $routeProvider.when('/story', {templateUrl: 'partials/story.html', controller: StoryCtrl});
     $routeProvider.otherwise({redirectTo: '/new-game'});
   }]);
