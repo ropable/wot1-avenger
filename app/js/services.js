08c0e58dbc2b5cf4c908970a09ccbb6319740405
Ashley Felton <ashley@ropable.com>
2013-04-13 07:58:21 +0800

    Added partial template include for character sheet.

--- app/js/services.js
+++ app/js/services.js
@@ -7,9 +7,20 @@ angular.module('wot1app.services', ['ngResource'])
         return $resource('data/story.json', {}, {});
     });
 
-wot1app.factory('gameState', function() {
+wot1app.factory('gameState', function($http) {
     var gameState = {};
     gameState.started = false;
+    gameState.punch = 0;
+    gameState.kick = 0;
+    gameState.throw = 0;
+    gameState.fate = 0;
+    gameState.innerForce = 5;
+    gameState.endurance = 20;
+    gameState.shuriken = 5;
+    // Get JSON for starting items.
+    $http.get('data/items.json').success(function(data) {
+        gameState.items = data;
+    });
     gameState.skills = [];
     return gameState;
 });
