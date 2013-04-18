999485ae743aeec4e5cb767e76e38cb990b4389b
Ashley Felton <ashley@ropable.com>
2013-04-18 21:55:25 +0800

    Finally learned about the ng-show directive.

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,10 @@
 var wot1app = angular.module('wot1', ['wot1app.services']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
+      when('/title', {templateUrl: 'partials/title.html'}).
       when('/new-game', {templateUrl: 'partials/new_game.html', controller: NewGameCtrl}).
       when('/background', {templateUrl: 'partials/background.html', controller: BackgroundCtrl}).
       when('/story', {templateUrl: 'partials/story.html', controller: StoryCtrl}).
-      when('/story/:entry', {templateUrl: 'partials/entry.html', controller: EntryCtrl}).
-      when('/title', {templateUrl: 'partials/title.html'}).
       otherwise({redirectTo: '/title'});
 
     //$locationProvider.html5Mode(true);
