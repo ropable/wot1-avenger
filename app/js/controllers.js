81ef566d408e3f55a4264391926fb2b08d8cf1d4
Ashley Felton <ashley@ropable.com>
2013-05-14 21:39:37 +0800

    Bunch of entries.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -131,7 +131,7 @@ function NewGameCtrl($scope, $http, localStorageService, Story, Items, Opponents
     $scope.beginGame = function() {
         localStorageService.clearAll();
         // Set starting entry number.
-        gameState.currentEntry = '1';
+        gameState.currentEntry = '408';
         gameState.endurance = 20;
         // Get starting items.
         angular.forEach(itemsjson, function(item) {
