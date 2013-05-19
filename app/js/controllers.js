fdbe1bbbc4231b4d46fcf52b56111b4b194aaca8
Ashley Felton <ashley@ropable.com>
2013-05-19 20:54:35 +0800

    Add cobra man image.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -140,7 +140,7 @@ function NewGameCtrl($scope, $http, localStorageService, Story, Items, Opponents
     $scope.beginGame = function() {
         localStorageService.clearAll();
         // Set starting entry number.
-        gameState.currentEntry = '323';
+        gameState.currentEntry = '65';
         gameState.endurance = 20;
         // Get starting items.
         angular.forEach(itemsjson, function(item) {
