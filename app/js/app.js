14edf3f61f353245da82a3166fbbeed90c1d09d8
Ashley Felton <ashley@ropable.com>
2013-05-28 07:29:40 +0800

    More entries (Fiendil).

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var wot1app = angular.module('wot1', ['wot1app.services', 'LocalStorageModule']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/title', {templateUrl: 'partials/title.html'}).
+      when('/new-game', {templateUrl: 'partials/new_game.html', controller: NewGameCtrl}).
+      when('/background', {templateUrl: 'partials/background.html'}).
+      when('/story', {templateUrl: 'partials/story.html', controller: StoryCtrl}).
+      otherwise({redirectTo: '/title'});
+
+    //$locationProvider.html5Mode(true);
+  }]);
