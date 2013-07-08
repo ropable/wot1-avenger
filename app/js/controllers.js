b2be9bfb7b18c206eca9a2549df7bb4a48c98e90
Ashley Felton <ashley@ropable.com>
2013-07-08 21:42:56 +0800

    Arena fight with Snow Giant.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -148,7 +148,7 @@ function NewGameCtrl($scope, $http, localStorageService, Story, Items, Opponents
     $scope.beginGame = function() {
         localStorageService.clearAll();
         // Set starting entry number.
-        gameState.currentEntry = '47';
+        gameState.currentEntry = '275';
         gameState.endurance = 20;
         // Get starting items.
         angular.forEach(itemsjson, function(item) {
